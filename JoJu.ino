aad5807318a66e90e915eca4e00017ca31376429
moeburn <moeburn@gmail.com>
2024-12-09 21:17:14 -0500

    Joju 2.0b1

--- JoJu.ino
+++ JoJu.ino
@@ -2,57 +2,34 @@
 #include <BlynkSimpleEsp32.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
+#include <ArduinoOTA.h>
 #include "time.h"
-#include <Adafruit_ADS1X15.h>
-#include <FastLED.h>
-#include <Preferences.h>
-#include <Wire.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <ADS1115_WE.h> 
 #include <Adafruit_INA219.h>
-
-
-#define ONE_WIRE_BUS 1
-
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-
+#include "Adafruit_SHT31.h"
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 Adafruit_INA219 ina219;
+#define I2C_ADDRESS 0x48
 
-Preferences preferences;
-
-#define LED_PIN 2
-
-#define NUM_LEDS 1
-CRGB leds[NUM_LEDS];
-
-Adafruit_ADS1115 ads;
-
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
 const char* ssid = "mikesnet";
 const char* password = "springchicken";
 
+#define CAMERA_PIN 0
+
 const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = -14400;  //Replace with your GMT offset (secs)
+const long gmtOffset_sec = -18000;  //Replace with your GMT offset (secs)
 const int daylightOffset_sec = 0;   //Replace with your daylight offset (secs)
 int hours, mins, secs;
-int zebraR, zebraG, zebraB, menuValue;
-int sliderValue = 255;
+bool camerapower = false;
+char auth[] = "fEZlZOio7CS1nBXNm3A8HR5DysrzIoYW";
 
 float tempSHT, humSHT;
 int16_t adc0, adc1, adc2, adc3, soil;
 float volts0, volts1, volts2, volts3;
 float wifi;
 
-bool buttonstart = false;
-bool ledon = false;
-bool needssaving = false;
 
   float shuntvoltage = 0;
   float busvoltage = 0;
@@ -60,9 +37,7 @@ bool needssaving = false;
   float loadvoltage = 0;
   float power_mW = 0;
 
-char auth[] = "fEZlZOio7CS1nBXNm3A8HR5DysrzIoYW";
 
-AsyncWebServer server(80);
 
 WidgetTerminal terminal(V10);
 
@@ -74,10 +49,6 @@ BLYNK_WRITE(V10) {
   if (String("help") == param.asStr()) {
     terminal.println("==List of available commands:==");
     terminal.println("wifi");
-    terminal.println("reset");
-    terminal.println("ledon");
-    terminal.println("ledoff");
-    terminal.println("soil");
     terminal.println("==End of list.==");
   }
   if (String("wifi") == param.asStr()) {
@@ -89,58 +60,18 @@ BLYNK_WRITE(V10) {
     terminal.println(WiFi.RSSI());
     printLocalTime();
   }
-  if (String("reset") == param.asStr()) {
-    terminal.println("Restarting...");
     terminal.flush();
-    ESP.restart();
-  }
-  if (String("ledon") == param.asStr()) {
-    terminal.println("Turning LED on.");
-    terminal.flush();
-    ledon = true;
-  }
-  if (String("ledoff") == param.asStr()) {
-    terminal.println("Turning LED on.");
-    terminal.flush();
-    ledon = false; 
-  }
-  if (String("soil") == param.asStr()) {
-    soil = ads.readADC_SingleEnded(3);
-    terminal.print("Soil: ");
-    terminal.println(soil);
-    terminal.flush();
-    
-  }
 }
 
-BLYNK_WRITE(V18)
-{
-     zebraR = param[0].asInt();
-     zebraG = param[1].asInt();
-     zebraB = param[2].asInt();
 
-}
-
-BLYNK_WRITE(V11)
-{
-  if (param.asInt() == 1) {buttonstart = true;}
-  if (param.asInt() == 0) {buttonstart = false;}
-}
 
 
 BLYNK_WRITE(V12)
 {
-   menuValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  if (param.asInt() == 1) {camerapower = true;}
+  if (param.asInt() == 0) {camerapower = false;}
 }
 
-BLYNK_WRITE(V13)
-{
-   sliderValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-}
-
-BLYNK_CONNECTED() {
-  Blynk.syncVirtual(V11);
-}
 
 void printLocalTime() {
   time_t rawtime;
@@ -150,160 +81,113 @@ void printLocalTime() {
   terminal.print(asctime(timeinfo));
 }
 
-void goToSleep(){
-    //esp_deep_sleep_enable_gpio_wakeup(1, ESP_GPIO_WAKEUP_GPIO_LOW);
-    //WiFi.disconnect();
-    //delay(1);
-    esp_sleep_enable_timer_wakeup(55000000); // 50 sec
-    esp_deep_sleep_start(); 
-    delay(1000);
+float readChannel(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while(adc.isBusy()){}
+  voltage = adc.getResult_V(); // alternative: getResult_mV for Millivolt
+  return voltage;
 }
 
 void setup(void) {
-  float tempinC = temperatureRead();
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  sensors.begin();
-  sensors.requestTemperatures(); 
-  tempSHT = sensors.getTempCByIndex(0);
-  ads.setGain(GAIN_ONE);  // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  ads.begin();
-  adc2 = ads.readADC_SingleEnded(0);
-  adc3 = ads.readADC_SingleEnded(1);
-  soil = ads.readADC_SingleEnded(3);
-  volts2 = ads.computeVolts(adc2)*2.0;
-  volts3 = ads.computeVolts(adc3)*2.0;
+  Serial.begin(115200);
+
+  Serial.println("ADC init");
+  Wire.begin();
+  adc.init();
+  adc.setVoltageRange_mV(ADS1115_RANGE_4096);
+  volts3 = 2.0 * readChannel(ADS1115_COMP_3_GND);
+  volts2 = 2.0 * readChannel(ADS1115_COMP_2_GND);
+  Serial.println("SHT init");
+  sht31.begin(0x44);
+  Serial.println("INA init");
   ina219.begin();
   ina219.setCalibration_16V_400mA();
-
+  tempSHT = sht31.readTemperature();
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power_mW = ina219.getPower_mW();
   loadvoltage = busvoltage + (shuntvoltage / 1000);
-
-
-  
-
+  pinMode(CAMERA_PIN, OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
-
-    
-  while ((WiFi.status() != WL_CONNECTED) && (millis() < 15000)) {
-      delay(250);
+  // Wait for connection
+  while ((WiFi.status() != WL_CONNECTED) && (millis()<20000)) {
+    if (millis() > 10000) {WiFi.setTxPower (WIFI_POWER_8_5dBm); Serial.print("!");}
+    delay(500);
+    Serial.print(".");
   }
-  wifi = WiFi.RSSI();
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  ArduinoOTA.begin();
+  Serial.println("HTTP server started");
+  delay(250);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   Blynk.config(auth, IPAddress(192, 168, 50, 197), 8080);
   Blynk.connect();
-  while ((!Blynk.connected()) && (millis() < 15000)){delay(250);}
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  
-
-
-  Blynk.virtualWrite(V1, tempSHT);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V2, tempinC);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V3, volts2);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V4, volts3);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V5, wifi);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V26, soil);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V21, shuntvoltage);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V22, busvoltage);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V23, current_mA);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V24, power_mW);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  Blynk.virtualWrite(V25, loadvoltage);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-
-  Blynk.virtualWrite(V25, loadvoltage);
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();} 
-   
-  
-  
-  
-  if (buttonstart) {
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-    struct tm timeinfo;
-    getLocalTime(&timeinfo);
-    hours = timeinfo.tm_hour;
-    mins = timeinfo.tm_min;
-    secs = timeinfo.tm_sec;
-    terminal.println("***JoJu 1.3 STARTED***");
-    terminal.print("Connected to ");
-    terminal.println(ssid);
-    terminal.print("IP address: ");
-    terminal.println(WiFi.localIP());
-    printLocalTime();
-    terminal.println(volts3,3);
-    
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(200, "text/plain", "Hi! I am ESP32.");
-    });
-
-    AsyncElegantOTA.begin(&server);    // Start ElegantOTA
-    server.begin();
-    terminal.println("HTTP server started");
-    terminal.flush();
-    if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-  }
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
-
-  //delay(500);
-  if (!buttonstart){
-    pinMode(LED_PIN, INPUT);
-    goToSleep();
-
-  }
-
-
-
+  delay(250);
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  hours = timeinfo.tm_hour;
+  mins = timeinfo.tm_min;
+  secs = timeinfo.tm_sec;
+  terminal.println("***JOJU 2.0 STARTED***");
+  terminal.print("Connected to ");
+  terminal.println(ssid);
+  terminal.print("IP address: ");
+  terminal.println(WiFi.localIP());
+  printLocalTime();
+  terminal.flush();
+            tempSHT = sht31.readTemperature();
+            volts3 = 2.0 * readChannel(ADS1115_COMP_3_GND);
+            volts2 = 2.0 * readChannel(ADS1115_COMP_2_GND);
+            shuntvoltage = ina219.getShuntVoltage_mV();
+            busvoltage = ina219.getBusVoltage_V();
+            current_mA = ina219.getCurrent_mA();
+            power_mW = ina219.getPower_mW();
+            loadvoltage = busvoltage + (shuntvoltage / 1000);
+            Blynk.virtualWrite(V1, tempSHT);
+            Blynk.virtualWrite(V3, volts2);
+            Blynk.virtualWrite(V4, volts3);
+            Blynk.virtualWrite(V5, WiFi.RSSI());
+            Blynk.virtualWrite(V21, shuntvoltage);
+            Blynk.virtualWrite(V22, busvoltage);
+            Blynk.virtualWrite(V23, current_mA);
+            Blynk.virtualWrite(V24, power_mW);
+            Blynk.virtualWrite(V25, loadvoltage);
 
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
+  Blynk.run();
+      if (camerapower) {digitalWrite(CAMERA_PIN, HIGH);}
+      else {digitalWrite(CAMERA_PIN, LOW);}
+      ArduinoOTA.handle(); 
+      every(10000) {
+            tempSHT = sht31.readTemperature();
+            volts3 = 2.0 * readChannel(ADS1115_COMP_3_GND);
+            volts2 = 2.0 * readChannel(ADS1115_COMP_2_GND);
+            shuntvoltage = ina219.getShuntVoltage_mV();
+            busvoltage = ina219.getBusVoltage_V();
+            current_mA = ina219.getCurrent_mA();
+            power_mW = ina219.getPower_mW();
+            loadvoltage = busvoltage + (shuntvoltage / 1000);
+            Blynk.virtualWrite(V1, tempSHT);
+            Blynk.virtualWrite(V3, volts2);
+            Blynk.virtualWrite(V4, volts3);
+            Blynk.virtualWrite(V5, WiFi.RSSI());
+            Blynk.virtualWrite(V21, shuntvoltage);
+            Blynk.virtualWrite(V22, busvoltage);
+            Blynk.virtualWrite(V23, current_mA);
+            Blynk.virtualWrite(V24, power_mW);
+            Blynk.virtualWrite(V25, loadvoltage);
 
-  every(5000){
-    sensors.requestTemperatures(); 
-    tempSHT = sensors.getTempCByIndex(0);
-    adc2 = ads.readADC_SingleEnded(0);
-    adc3 = ads.readADC_SingleEnded(1);
-    soil = ads.readADC_SingleEnded(3);
-    volts2 = ads.computeVolts(adc2)*2.0;
-    volts3 = ads.computeVolts(adc3)*2.0;
-    shuntvoltage = ina219.getShuntVoltage_mV();
-    busvoltage = ina219.getBusVoltage_V();
-    current_mA = ina219.getCurrent_mA();
-    power_mW = ina219.getPower_mW();
-    loadvoltage = busvoltage + (shuntvoltage / 1000);
-    Blynk.virtualWrite(V1, tempSHT);
-    //Blynk.virtualWrite(V2, humSHT);
-    Blynk.virtualWrite(V3, volts2);
-    Blynk.virtualWrite(V4, volts3);
-    Blynk.virtualWrite(V5, WiFi.RSSI());
-    Blynk.virtualWrite(V21, shuntvoltage);
-    Blynk.virtualWrite(V22, busvoltage);
-    Blynk.virtualWrite(V23, current_mA);
-    Blynk.virtualWrite(V24, power_mW);
-    Blynk.virtualWrite(V25, loadvoltage);
-    Blynk.virtualWrite(V26, soil);
-  }
-
-
-    every(10){
-      leds[0] = CRGB(zebraR, zebraG, zebraB);
-      FastLED.setBrightness(sliderValue);
-      FastLED.show();
-    }
-
+      }
 }
